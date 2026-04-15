cb8e7f187ffd97e5170a4f3039ae33e507d03929
HussainAnjan5 <175691866+HussainAnjan5@users.noreply.github.com>
2026-04-15 20:12:04 +0500

    Remove extra blank lines in About component

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -48,6 +48,8 @@ export const About = () => {
                       <th scope="row">{data.jobtitle}</th>
                       <td>{data.where}</td>
                       <td>{data.date}</td>
+    
+    
                     </tr>
                   );
                 })}
